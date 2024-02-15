9daff7be64e005cdf7eb00c6bae1d8ff21f9759e
vigarcia <vigarcia828@gmail.com>
2024-02-15 13:39:53 -0800

    comment

--- myscript.js
+++ myscript.js
@@ -1,1 +1,29 @@
-   <!-- QUIZ -->
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $(".codigo").toggle();
+  });
+
+
+
+  $(".button2").click(function(){
+    $("p").filter(".clase").toggle();
+  });
+   $(".button3").click(function(){
+    $("p").filter(".adios").toggle();
+  });
+   $(".button4").click(function(){
+    $("p").filter(".computadora").toggle();
+  });
+   $(".button5").click(function(){
+    $("p").filter(".familia").toggle();
+  });
+   $(".button6").click(function(){
+    $("p").filter(".libro").toggle();
+  });
+   $(".button7").click(function(){
+    $("p").filter(".gato").toggle();
+  });
+});
+
+
+
